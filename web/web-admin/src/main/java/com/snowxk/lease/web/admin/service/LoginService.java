c791fb2b7c58d55019954833f7a97aee057dbfac
SnoWxK <2628590807@qq.com>
2026-04-22 20:20:17 +0800

    基础代码导入

---
+++ web/web-admin/src/main/java/com/snowxk/lease/web/admin/service/LoginService.java
@@ -0,0 +1,9 @@
+package com.snowxk.lease.web.admin.service;
+
+import com.snowxk.lease.web.admin.vo.login.CaptchaVo;
+import com.snowxk.lease.web.admin.vo.login.LoginVo;
+import com.snowxk.lease.web.admin.vo.system.user.SystemUserInfoVo;
+
+public interface LoginService {
+
+}
---
+++ web/web-admin/src/main/java/com/snowxk/lease/web/admin/service/impl/FileServiceImpl.java
@@ -0,0 +1,9 @@
+package com.snowxk.lease.web.admin.service.impl;
+
+import com.snowxk.lease.web.admin.service.FileService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+}
