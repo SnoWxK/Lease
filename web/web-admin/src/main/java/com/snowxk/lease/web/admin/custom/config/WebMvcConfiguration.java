03bbd86a3ba8f985f0179653e02a21303a4949a2
SnoWxK <2628590807@qq.com>
2026-04-24 11:57:43 +0800

    标签相关接口

--- web/web-admin/src/main/java/com/snowxk/lease/web/admin/custom/config/WebMvcConfiguration.java
+++ web/web-admin/src/main/java/com/snowxk/lease/web/admin/custom/config/WebMvcConfiguration.java
@@ -1,6 +1,6 @@
 package com.snowxk.lease.web.admin.custom.config;
 
-import com.snowxk.lease.web.admin.custom.converter.StringToItemTypeConverter;
+import com.snowxk.lease.web.admin.custom.converter.StringToBaseEnumConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -8,12 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-
     @Autowired
-    private StringToItemTypeConverter stringToItemTypeConverter;
+    private StringToBaseEnumConverter stringToBaseEnumConverter;
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(this.stringToItemTypeConverter);
+        registry.addConverterFactory(stringToBaseEnumConverter);
     }
 }
