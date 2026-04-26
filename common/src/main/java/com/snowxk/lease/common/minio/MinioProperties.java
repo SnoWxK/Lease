31f2595dbecac853f23e339820682467ac82d10a
SnoWxK <2628590807@qq.com>
2026-04-26 09:44:35 +0800

    上传图片

---
+++ common/src/main/java/com/snowxk/lease/common/minio/MinioProperties.java
@@ -0,0 +1,15 @@
+package com.snowxk.lease.common.minio;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "minio")
+@Data
+public class MinioProperties {
+
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+}
--- web/web-admin/src/main/java/com/snowxk/lease/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/snowxk/lease/web/admin/service/impl/FileServiceImpl.java
@@ -1,9 +1,73 @@
 package com.snowxk.lease.web.admin.service.impl;
 
+import com.snowxk.lease.common.minio.MinioProperties;
 import com.snowxk.lease.web.admin.service.FileService;
+import io.minio.*;
+import io.minio.errors.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Service
 public class FileServiceImpl implements FileService {
 
+    @Autowired
+    private MinioClient minioClient;
+    @Autowired
+    private MinioProperties properties;
+
+    @Override
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+            boolean bucketExists = minioClient.bucketExists(
+                    BucketExistsArgs.builder()
+                            .bucket(properties.getBucketName())
+                            .build());
+            if (!bucketExists) {
+                minioClient.makeBucket(
+                        MakeBucketArgs.builder()
+                                .bucket(properties.getBucketName())
+                                .build());
+                minioClient.setBucketPolicy(
+                        SetBucketPolicyArgs.builder()
+                                .bucket(properties.getBucketName())
+                                .config(createBucketPolicyConfig(properties.getBucketName()))
+                                .build());
+            }
+            String datePath = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+            String originalName = file.getOriginalFilename();
+            String filename = datePath + "/" + UUID.randomUUID() + "-" + originalName;
+
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(properties.getBucketName())
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .object(filename)
+                            .contentType(file.getContentType())
+                            .build());
+
+            return String.join("/", properties.getEndpoint(), properties.getBucketName(), filename);
+    }
+
+
+    private String createBucketPolicyConfig(String bucketName) {
+
+        return """
+                {
+                  "Statement" : [ {
+                    "Action" : "s3:GetObject",
+                    "Effect" : "Allow",
+                    "Principal" : "*",
+                    "Resource" : "arn:aws:s3:::%s/*"
+                  } ],
+                  "Version" : "2012-10-17"
+                }
+                """.formatted(bucketName);
+    }
 }
