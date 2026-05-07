7ca4b44c56abf4caca5e61d9961e6974881a543a
SnoWxK <2628590807@qq.com>
2026-05-07 18:26:17 +0800

    注册与登录接口

--- common/src/main/java/com/snowxk/lease/common/utils/CodeUtil.java
+++ common/src/main/java/com/snowxk/lease/common/utils/CodeUtil.java
@@ -4,12 +4,14 @@ import java.util.Random;
 
 public class CodeUtil {
     public static String getRandomCode(Integer length) {
-        StringBuilder builder = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < length; i++) {
-            StringBuilder num = builder.append(random.nextInt(10));
-            builder.append(num);
-        }
-        return builder.toString();
+        return "123456";
+//        由于阿里云短信认证不对个人开放，直接修改验证码为固定值
+//        StringBuilder builder = new StringBuilder();
+//        Random random = new Random();
+//        for (int i = 0; i < length; i++) {
+//            StringBuilder num = builder.append(random.nextInt(10));
+//            builder.append(num);
+//        }
+//        return builder.toString();
     }
 }
--- web/web-app/src/main/java/com/snowxk/lease/web/app/service/LoginService.java
+++ web/web-app/src/main/java/com/snowxk/lease/web/app/service/LoginService.java
@@ -1,5 +1,9 @@
 package com.snowxk.lease.web.app.service;
 
+import com.snowxk.lease.web.app.vo.user.LoginVo;
+
 public interface LoginService {
     void getCode(String phone);
+
+    String login(LoginVo loginVo);
 }
